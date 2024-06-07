eb460d402a491723ad20a3f81d40d8103f0fe65b
Jahir Celorio <jahir.celorio200@gmail.com>
2024-06-07 10:44:40 -0500

    Creacion de migraciones para contacts y user con validacion

---
+++ database/migrations/2024_06_07_151930_create_contact_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('nickname')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            // Para que el usuario solo sea el celular unico solo para el usuario que lo creo 
+
+            $table->unique(['user_id', 'phone']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact');
+    }
+};
