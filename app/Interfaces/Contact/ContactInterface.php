6d15c7e8ae6f2567f8d13977c80ca23412c5f963
Jahir Celorio <jahir.celorio200@gmail.com>
2024-06-11 15:36:22 -0500

    create interface Contact

---
+++ app/Interfaces/Contact/ContactInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Interfaces\Contact;
+use Illuminate\Http\Request;
+
+interface ContactInterface{
+    public function index();
+    public function store(Request $request);
+    public function show($id);
+    public function update(Request $request, $id);
+    public function dleete($id);
+}
