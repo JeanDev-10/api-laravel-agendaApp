c0fcef4e356bac3828d7c4d663a6ec78eb794ed2
Jean10112002 <jeanrodriguez060420@gmail.com>
2024-07-04 23:09:41 -0500

    refactor: :fire: policies for autorization in update or delete contacts in base to the user

---
+++ app/Policies/ContactPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Contact;
+use App\Models\User;
+
+class ContactPolicy
+{
+
+    public function update(User $user, Contact $contact): bool
+    {
+        return $user->id === $contact->user_id;
+    }
+
+    public function delete(User $user, Contact $contact): bool
+    {
+        return $user->id === $contact->user_id;
+    }
+}
