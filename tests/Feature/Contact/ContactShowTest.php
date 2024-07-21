318e9672ef33aa0d6a6242cd0ae71ed0fe5ef53f
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2024-07-20 20:06:02 -0500

    test:

--- tests/Feature/Contact/ContactShowTest.php
+++ tests/Feature/Contact/ContactShowTest.php
@@ -44,5 +44,29 @@ class ContactShowTest extends TestCase
         $response->assertJsonPath('data.name', $contact->name);
         $this->assertDatabaseHas('contacts', $contact->toArray());
     }
+    public function test_user_can_no_get_a_contact_other_person(): void
+    {
+        $user = User::factory()->create();
+        $contact = Contact::factory([
+            "user_id" => $user->id
+        ])->create();
+        $user2 = User::factory()->create();
+        $this->assertDatabaseHas('contacts', $contact->toArray());
+        $token = JWTAuth::fromUser($user2);
+        $encrypted_id = Crypt::encrypt($contact->id);
+        $response = $this->withHeaders([
+            "Authorization" => "Bearer $token"
+        ])->getJson('api/v1/contact/'.$encrypted_id);
+        $response->assertStatus(403);
+        $response->assertJsonStructure([
+            'message',
+            'statusCode',
+            'error',
+            'data'
+        ]);
+        $response->assertJson([
+            "message" => "No estás autorizado para ver este contacto"
+        ]);
+    }
 
 }
